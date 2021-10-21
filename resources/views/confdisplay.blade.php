f931acc55377e615177c5447ddb63a2a7861b279
Sashou <padua_skye@plpasig.edu.ph>
2021-10-21 16:41:14 +0800

    added simple booking confirmed email format

--- resources/views/confdisplay.blade.php
+++ resources/views/confdisplay.blade.php
@@ -5,11 +5,23 @@
 {{-- @if (session()->exists('confirmation_number')) --}}
 
 
-<p>Booked Successfully!</p>
+<p>Booked Successfully! Welcome to Mondstadt!</p>
 <p>Reservation Number: {{session('confirmation_number')}}</p>
-<p>do take note chuchu</p>
 <p>total price: PHP {{number_format(session('overallprice'), 2)}}</p>
 <p>downpayment: PHP {{number_format(session('overallprice') * 0.5, 2)}}</p>
+<hr>
+
+<p>- Proceed to the desk after arrival</p>
+<p>- Present your Reservation Number and Valid Id to confirm your identity </p>
+<p>- After confirming your reservation please provide the follow up payment </p>
+
+<p> ************************************** </p>
+<h5>Cancellation Policy</h5>
+<hr>
+<p> Cancellation must be done 48 hours prior to your arrival doing so will not result of any charge </p>
+<p> Cancellation after the given duration or failing to show will cause a one night fee via your Payment Method </p>
+
+
 {{--
 @else
 
