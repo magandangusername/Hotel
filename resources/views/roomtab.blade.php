c501cdb17527e0bf3d11f29501cb53d1bc75e71f
Matthew <perez_johnmatthewleonard@plpasig.edu.ph>
2021-11-06 00:23:43 +0800

    room suite frontend adjustment

--- resources/views/roomtab.blade.php
+++ resources/views/roomtab.blade.php
@@ -2,20 +2,21 @@
 
 @section('content')
 
-            <div class="container-fluid p-0 m-0">
-                <div class="text-center roomshowcase">
-                <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+    <div class="container-fluid p-0 m-0">
+        <div class="text-center roomshowcase">
+            <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
 
                 <div class="d-flex justify-content-center align-items-center p-5">
                     <div class="p-5 m-5 text-white">
                         <h1 class="mb-3 fw-bold">Rooms</h1>
-                        <h4 class="mb-3">Each of our 70 rooms and suites offers luxury and elegance, efficiency and comfort.</h4>
+                        <h4 class="mb-3">Each of our 70 rooms and suites offers luxury and elegance, efficiency and
+                            comfort.</h4>
                     </div>
                 </div>
 
-                </div>
-             </div>
-           </div>
+            </div>
+        </div>
+    </div>
 
 
 
@@ -23,51 +24,128 @@
 
 
     <div class="container-fluid sectioncolor p-0 m-0">
-    <div class="row  sectioncolor">
+        <div class="row  sectioncolor">
 
-        <div class="col mt-5 text-center">
-        <div class="row pt-5">
-            <div class="col">
-            <h1 class="fw-bold">this is a text</h1>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-            <p class="abouts1"> this is a text</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-            <form method="POST" action="">
-            <a href="" class="text-decoration-none btn btn-light fw-bold">Learn More</a>
-            <a href="" class="btn btn-light fw-bold"  name="booknow"> Book Now</a>
-            </form>
-            </div>
-        </div>
-        </div>
-
-<div class="col">
-  <img src=""  class="img-thumbnail">
-</div>
-
-</div>
-            <!-- <div class="row">
-                @foreach ($rooms as $room)
+            {{-- <div class="col mt-5 text-center">
+                <div class="row pt-5">
                     <div class="col">
-                        <img src="{{ asset('images/' . $room->image_name) }}">
+                        <h1 class="fw-bold">this is a text</h1>
                     </div>
-
-                    <div class="col ">
-                        <a href="{{ url('roomtab') }}/{{ $room->room_name }}"></a>
-                        <h4 class="bold">{{ $room->room_name }}</h4>
-                        <p>{{ $room->room_short_description }}</p>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p class="abouts1"> this is a text</p>
                     </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <form method="POST" action="">
+                            <a href="" class="text-decoration-none btn btn-light fw-bold">Learn More</a>
+                            <a href="" class="btn btn-light fw-bold" name="booknow"> Book Now</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
 
-                @endforeach
+            <div class="col">
+                <img src="" class="img-thumbnail">
+            </div> --}}
+
+        </div>
+        <div class="row">
+            {{-- @foreach ($rooms as $room)
+                <div class="col">
+                    <img src="{{ asset('images/' . $room->image_name) }}">
+                </div>
+
+                <div class="col ">
+                    <a href="{{ url('roomtab') }}/{{ $room->room_name }}"></a>
+                    <h4 class="bold">{{ $room->room_name }}</h4>
+                    <p>{{ $room->room_short_description }}</p>
+                </div>
+
+            @endforeach --}}
+
+            @php
+            $rows = 0;
+            @endphp
+            @foreach ($rooms as $room)
+            @php
+                $rows += 1;
+            @endphp
+
+            @if ($rows % 2 ==1)
+                <!-- ODD -->
+
+                    <div class="container mt-3 px-5 pb-5">
+                    <div class="row  sectioncolor">
+
+                        <div class="col mt-5 text-center">
+                        <div class="row pt-5">
+                            <div class="col">
+                            <h1 class="fw-bold">{{ $room->room_name }}</h1>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                            <p class="abouts1"> {{ $room->room_short_description }} </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                            <form method="POST" action="">
+                            <a href="{{ url('roomtab') }}/{{ $room->room_name }}" class="text-decoration-none btn btn-light fw-bold">Learn More</a>
+                            {{-- <a href="{{ route('book.index') }}?promocode={{ $promo->promotion_code }}" class="btn btn-light fw-bold" id="promobonus" name="booknow"> Book Now</a> --}}
+                            </form>
+                            </div>
+                        </div>
+                        </div>
+
+                        <div class="col">
+                        <img src="{{ asset('images/'.$room->image_name) }}"  class="img-thumbnail">
+                        </div>
+
+                    </div>
+                    </div>
+            @endif
+
+            @if ($rows % 2 == 0)
+                <!-- EVEN -->
+                    <div class="container mt-3 px-5 text-center">
+                    <div class="row sectioncolor">
+                        <div class="col">
+                        <img src="{{ asset('images/'.$room->image_name) }}" class="img-thumbnail">
+                        </div>
+                        <div class="col mt-5 text-center">
+                        <div class="row pt-5">
+                            <div class="col">
+                            <h1 class="fw-bold">{{ $room->room_name }}</h1>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                            <p class="abouts1"> {{ $room->room_short_description }} </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                            <form method="POST" action="">
+                            <a href="{{ route('roomtab') }}/{{ $room->room_name }}" class="text-decoration-none btn btn-light fw-bold">Learn More</a>
+                            {{-- <a href="{{ route('book.index') }}?promocode={{ $promo->promotion_code }}" class="btn btn-light fw-bold" id="promobonus" name="booknow"> Book Now</a> --}}
+                            </form>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+            @endif
+            @endforeach
 
 
-                idk what this does
-                {{-- <div class="col"></div>
+            {{-- idk what this does
+
+            yeah just ignore this idk either lol--}}
+            {{-- <div class="col"></div>
                 <div class="col" id="availnow">
                     <h4 class="bold"> Room</h3>
                         <p>this is standard</p>
@@ -75,7 +153,7 @@
                 </div>
                 @include('layouts.checkavailability2') --}}
 
-            </div> -->
         </div>
+    </div>
 
 @endsection
--- resources/views/suitestab.blade.php
+++ resources/views/suitestab.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <!---------------------------------slider------------------------------->
+    {{-- <!---------------------------------slider------------------------------->
     <section id="slider">
         <div class="col mx-auto d-block">
             <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
@@ -58,15 +58,135 @@
                     </div>
                 @endforeach
 
-                <!-- {{-- <div class="col"></div>
+                <!-- <div class="col"></div>
                 <div class="col-lg-2" id="availnow">
                     <h4 class="bold"> Room</h3>
                         <p>this is standard</p>
                         <a href="">see latest room offers</a>
                 </div>
 
-                @include('layouts.checkavailability2') --}} -->
+                @include('layouts.checkavailability2') -->
+
+            </div>
+        </div> --}}
+
+
+
+
+
+
+
+
+
+
+    <div class="container-fluid p-0 m-0">
+        <div class="text-center roomshowcase">
+            <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+
+                <div class="d-flex justify-content-center align-items-center p-5">
+                    <div class="p-5 m-5 text-white">
+                        <h1 class="mb-3 fw-bold">Suites</h1>
+                        <h4 class="mb-3">Each of our 70 rooms and suites offers luxury and elegance, efficiency and
+                            comfort.</h4>
+                    </div>
+                </div>
 
             </div>
         </div>
+    </div>
+
+
+
+    <!---------------------------------rooms------------------------------->
+
+
+    <div class="container-fluid sectioncolor p-0 m-0">
+        <div class="row  sectioncolor">
+
+
+        </div>
+        <div class="row">
+            @php
+                $rows = 0;
+            @endphp
+            @foreach ($suites as $suite)
+                @php
+                    $rows += 1;
+                @endphp
+
+                @if ($rows % 2 == 1)
+                    <!-- ODD -->
+
+                    <div class="container mt-3 px-5 pb-5">
+                        <div class="row  sectioncolor">
+
+                            <div class="col mt-5 text-center">
+                                <div class="row pt-5">
+                                    <div class="col">
+                                        <h1 class="fw-bold">{{ $suite->suite_name }}</h1>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <p class="abouts1"> {{ $suite->suite_short_description }} </p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <form method="POST" action="">
+                                            <a href="{{ url('suitestab') }}/{{ $suite->suite_name }}"
+                                                class="text-decoration-none btn btn-light fw-bold">Learn More</a>
+                                            {{-- <a href="{{ route('book.index') }}?promocode={{ $promo->promotion_code }}" class="btn btn-light fw-bold" id="promobonus" name="booknow"> Book Now</a> --}}
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col">
+                                <img src="{{ asset('images/' . $suite->image_name) }}" class="img-thumbnail">
+                            </div>
+
+                        </div>
+                    </div>
+                @endif
+
+                @if ($rows % 2 == 0)
+                    <!-- EVEN -->
+                    <div class="container mt-3 px-5 text-center">
+                        <div class="row sectioncolor">
+                            <div class="col">
+                                <img src="{{ asset('images/' . $suite->image_name) }}" class="img-thumbnail">
+                            </div>
+                            <div class="col mt-5 text-center">
+                                <div class="row pt-5">
+                                    <div class="col">
+                                        <h1 class="fw-bold">{{ $suite->suite_name }}</h1>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <p class="abouts1"> {{ $suite->suite_short_description }} </p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <form method="POST" action="">
+                                            <a href="{{ route('roomtab') }}/{{ $suite->suite_name }}"
+                                                class="text-decoration-none btn btn-light fw-bold">Learn More</a>
+                                            {{-- <a href="{{ route('book.index') }}?promocode={{ $promo->promotion_code }}" class="btn btn-light fw-bold" id="promobonus" name="booknow"> Book Now</a> --}}
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+            @endforeach
+
+
+
+        </div>
+    </div>
+
+
 @endsection
