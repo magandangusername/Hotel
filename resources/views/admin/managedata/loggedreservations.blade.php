2e824e8eb96dcc823ba3660315b935572ec11dfe
Sashou <padua_skye@plpasig.edu.ph>
2021-11-12 02:25:37 +0800

    admin layout v0.01

--- resources/views/admin/managedata/loggedreservations.blade.php
+++ resources/views/admin/managedata/loggedreservations.blade.php
@@ -3,71 +3,71 @@
 @section('content')
 
 <main>
-                    <div class="container-fluid px-4">
+    <div class="container-fluid px-4">
 
-                        <div class="card my-5">
-                            <div class="card-header">
+        <div class="card my-5">
+            <div class="card-header">
 
-                               <h2>Past Reservations </h2>
-                            </div>
-                            <div class="card-body">
-                                <table id="datatablerr">
-                                    <thead>
-                                        <tr class="text-light bg-dark">
-                                            <th>Reservation Number</th>
-                                            <th>Guest Code</th>
-                                            <th>Payment Code</th>
-                                            <th>Arrival Date</th>
-                                            <th>Departure Date</th>
-                                            <th>Booked at</th>
-                                            <th>Log Status</th>
-                                            <th>End At</th>
-                                        </tr>
-                                    </thead>
+                <h2>Past Reservations </h2>
+            </div>
+            <div class="card-body">
+                <table id="datatablerr">
+                    <thead>
+                        <tr class="text-light bg-dark">
+                            <th>Reservation Number</th>
+                            <th>Guest Code</th>
+                            <th>Payment Code</th>
+                            <th>Arrival Date</th>
+                            <th>Departure Date</th>
+                            <th>Booked at</th>
+                            <th>Log Status</th>
+                            <th>End At</th>
+                        </tr>
+                    </thead>
 
-                                    <tbody>
-                                        <tr>
-                                            <td>21303182471</td>
-                                            <td>01/10/21</td>
-                                            <td>Too Dumb</td>
-                                            <td>21303182471</td>
-                                            <td>01/10/21</td>
-                                            <td>Too Dumb</td>
-                                            <td>21303182471</td>
-                                            <td>01/10/21</td>
+                    <tbody>
+                        <tr>
+                            <td>21303182471</td>
+                            <td>01/10/21</td>
+                            <td>Too Dumb</td>
+                            <td>21303182471</td>
+                            <td>01/10/21</td>
+                            <td>Too Dumb</td>
+                            <td>21303182471</td>
+                            <td>01/10/21</td>
 
 
 
-                                            <td>
-                                                <button class="btn btn-outline-dark" type="submit"><i class="fas fa-trash"></i></button>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>21303182471</td>
-                                            <td>01/10/21</td>
-                                            <td>My wife left me</td>
-                                            <td>21303182471</td>
-                                            <td>01/10/21</td>
-                                            <td>Too Dumb</td>
-                                            <td>21303182471</td>
-                                            <td>01/10/21</td>
+                            <td>
+                                <button class="btn btn-outline-dark" type="submit"><i class="fas fa-trash"></i></button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>21303182471</td>
+                            <td>01/10/21</td>
+                            <td>My wife left me</td>
+                            <td>21303182471</td>
+                            <td>01/10/21</td>
+                            <td>Too Dumb</td>
+                            <td>21303182471</td>
+                            <td>01/10/21</td>
 
-                                            <td>
-                                                <button class="btn btn-outline-dark" type="submit"><i class="fas fa-trash"></i></button>
-                                            </td>
-                                        </tr>
+                            <td>
+                                <button class="btn btn-outline-dark" type="submit"><i class="fas fa-trash"></i></button>
+                            </td>
+                        </tr>
 
-                                    </tbody>
+                    </tbody>
 
-                                </table>
+                </table>
 
-                            </div>
+            </div>
 
-                        </div>
+        </div>
 
-                    </div>
+    </div>
 
-                </main>
+</main>
 
 
 
--- resources/views/admin/managedata/paymentdata.blade.php
+++ resources/views/admin/managedata/paymentdata.blade.php
@@ -4,56 +4,56 @@
 
 <div class="container-fluid px-4">
 
-<div class="card my-5">
-    <div class="card-header">
-       <h2>Payments </h2>
+    <div class="card my-5">
+        <div class="card-header">
+            <h2>Payments </h2>
+        </div>
+        <div class="card-body">
+            <table id="datatablerr">
+                <thead>
+                    <tr class="text-light bg-dark">
+                        <th>Payment Code</th>
+                        <th>Name on Card</th>
+                        <th>Card Number</th>
+                        <th>Card Brand</th>
+                        <th>Expiration</th>
+
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>PM-01</td>
+                        <td> Mark</td>
+                        <td>213123123123123</td>
+                        <td>Ligma </td>
+                        <td>01/10</td>
+
+
+                        <td>
+                            <button class="btn btn-outline-dark" type="submit"><i class="fas fa-trash"></i></button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>PM-01</td>
+                        <td> Mark</td>
+                        <td>213123123123123</td>
+                        <td>Ligma </td>
+                        <td>01/10</td>
+
+
+                        <td>
+                            <button class="btn btn-outline-dark" type="submit"><i class="fas fa-trash"></i></button>
+                        </td>
+                    </tr>
+
+                </tbody>
+
+            </table>
+
+        </div>
+
     </div>
-    <div class="card-body">
-        <table id="datatablerr">
-            <thead>
-                <tr class="text-light bg-dark">
-                    <th>Payment Code</th>
-                    <th>Name on Card</th>
-                    <th>Card Number</th>
-                    <th>Card Brand</th>
-                    <th>Expiration</th>
-
-                </tr>
-            </thead>
-
-            <tbody>
-                <tr>
-                    <td>PM-01</td>
-                    <td> Mark</td>
-                    <td>213123123123123</td>
-                    <td>Ligma </td>
-                    <td>01/10</td>
-
-
-                    <td>
-                        <button class="btn btn-outline-dark" type="submit"><i class="fas fa-trash"></i></button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>PM-01</td>
-                    <td> Mark</td>
-                    <td>213123123123123</td>
-                    <td>Ligma </td>
-                    <td>01/10</td>
-
-
-                    <td>
-                        <button class="btn btn-outline-dark" type="submit"><i class="fas fa-trash"></i></button>
-                    </td>
-                </tr>
-
-            </tbody>
-
-        </table>
-
-    </div>
-
-</div>
 
 
 </div>
