93eabb69c75ca75c00b374d8985530579d74b45f
Sashou <padua_skye@plpasig.edu.ph>
2021-11-15 01:08:20 +0800

    email layouts, new admin reservation layout

--- resources/views/admin/modification.blade.php
+++ resources/views/admin/modification.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ Email Subject }}</title>
     <style>
         html,
         body,
